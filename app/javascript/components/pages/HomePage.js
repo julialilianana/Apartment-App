d5b65b9ae4697a304f8b63abd84627153ea2c701
Ubuntu <ubuntu@ip-172-31-17-147.us-west-2.compute.internal>
2020-04-01 20:53:39 +0000

    working home page

--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-class Home extends React.Component {
+class HomePage extends React.Component {
   render() {
     return(
       <h1>This is the Home Page</h1>
@@ -8,4 +8,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home
+export default HomePage
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_202905) do
+ActiveRecord::Schema.define(version: 2020_03_31_230352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.integer "postal_code"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
